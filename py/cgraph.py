dc0f93d6660fdcb1bc9ae89cf54c8dd5107e2c84
Mengsu Chen <MengsuChen@gmail.com>
2018-06-25 16:07:53 -0400

    add questions nodes and users nodes to stackoverflow-neo4j

--- py/cgraph.py
+++ py/cgraph.py
@@ -1,9 +1,12 @@
 """clone graph stored to neo4j"""
 from . import dbinfo
+from . import labeled
+
 import importlib
 import psycopg2
 
 importlib.reload(dbinfo)
+importlib.reload(labeled)
 
 def clear_all():
     with dbinfo.driver.session() as session:
@@ -21,10 +24,46 @@ def create_cid_nodes(filter_str="TRUE"):
 def create_answer_nodes(filter_str="TRUE"):
     with dbinfo.driver.session() as session:
         tx = session.begin_transaction()
-        rows = get_unique_postids(dbinfo.tb_name_suffix, dbinfo.connect_str, filter_str)
+        rows = get_unique_aids(dbinfo.tb_name_suffix, dbinfo.connect_str, filter_str)
         for row in rows:
-            tx.run("CREATE (:Answer {id:%d})" % row[0])
+            cypher = """MATCH (q:Question {id:%d})
+                        CREATE (q)-[:Has]->(:Answer {id:%d})
+                    """ % (row[0], row[1])
+            tx.run(cypher)
         tx.commit()
+
+
+def create_question_nodes(filter_str="TRUE"):
+    with dbinfo.driver.session() as session:
+        tx = session.begin_transaction()
+        rows = get_unique_qids(dbinfo.tb_name_suffix, dbinfo.connect_str, filter_str)
+        for row in rows:
+            tx.run("CREATE (:Question {id:%d})" % row[0])
+        tx.commit()
+
+
+def create_user_nodes(filter_str="TRUE"):
+    with dbinfo.driver.session() as session:
+        tx = session.begin_transaction()
+        rows = get_ans_usr(dbinfo.tb_name_suffix, dbinfo.connect_str, filter_str)
+        for row in rows:
+            cypher = """MATCH (ans:Answer {id: %d})
+                        MERGE (usr:User {id: %d})
+                        ON CREATE SET usr.id = %d
+                        CREATE UNIQUE (usr)-[:Offer]->(ans)
+                     """ % (row[0], row[1], row[1])
+            tx.run(cypher)
+
+        rows = get_qst_usr(dbinfo.tb_name_suffix, dbinfo.connect_str, filter_str)
+        for row in rows:
+            cypher = """MATCH (qst:Question {id: %d})
+                        MERGE (usr:User {id: %d})
+                        ON CREATE SET usr.id = %d
+                        CREATE UNIQUE (usr)-[:Ask]->(qst)
+                     """ % (row[0], row[1], row[1])
+            tx.run(cypher)
+        tx.commit()
+
 
 def create_snippet_nodes(filter_str="TRUE"):
     with dbinfo.driver.session() as session:
@@ -66,17 +105,112 @@ def get_unique_cids(tb_name_suffix, connect_str, filter_str="TRUE"):
         rows = cursor.fetchall()
         return rows
     
-def get_unique_postids(tb_name_suffix, connect_str, filter_str="TRUE"):
+def get_unique_aids(tb_name_suffix=dbinfo.tb_name_suffix, connect_str=dbinfo.connect_str, filter_str="TRUE"):
+    '''get all labeled answers'''
     with psycopg2.connect(connect_str) as conn:
         cursor = conn.cursor()
-        sql = """SELECT DISTINCT p.postid0
+        sql = """SELECT DISTINCT posts.parentid, posts.id
+                 FROM posts
+                 INNER JOIN        
+                (SELECT DISTINCT p.postid0 AS postid
                 FROM labels
                 INNER JOIN clone_pairs_%s p
                 ON labels.postid  = p.postid0
                 AND labels.indx = p.indx0
                 AND labels.tbegin = p.tbegin0
                 AND labels.tend = p.tend0
-                WHERE %s
+                WHERE %s) answers
+                ON posts.id = answers.postid
+                ;
+                """ % (tb_name_suffix, filter_str)
+        cursor.execute(sql)        
+        rows = cursor.fetchall()
+        return rows
+
+def get_qst_usr(tb_name_suffix=dbinfo.tb_name_suffix, connect_str=dbinfo.connect_str, filter_str="TRUE"):
+    '''get all labeled answers'''
+    with psycopg2.connect(connect_str) as conn:
+        cursor = conn.cursor()
+        sql = """SELECT DISTINCT posts.id, posts.owneruserid
+                FROM posts
+                INNER JOIN
+                (SELECT DISTINCT posts.parentid AS postid
+                 FROM posts
+                 INNER JOIN        
+                    (SELECT DISTINCT p.postid0 AS postid
+                    FROM labels
+                    INNER JOIN clone_pairs_%s p
+                    ON labels.postid  = p.postid0
+                    AND labels.indx = p.indx0
+                    AND labels.tbegin = p.tbegin0
+                    AND labels.tend = p.tend0
+                    WHERE %s) AS answers
+                 ON posts.id = answers.postid) AS questions
+                ON posts.id = questions.postid
+                ;
+                """ % (tb_name_suffix, filter_str)
+        cursor.execute(sql)        
+        rows = cursor.fetchall()
+        replaced_rows = []
+        n_none_uid = 0
+        for aid, uid in rows:
+            if uid is None:
+                n_none_uid += 1
+                #print(aid, "doesn't have owner")
+            else:
+                replaced_rows.append((aid, uid))
+        print("there are", n_none_uid, "questions that don't have owner")
+        return replaced_rows
+
+
+
+def get_ans_usr(tb_name_suffix=dbinfo.tb_name_suffix, connect_str=dbinfo.connect_str, filter_str="TRUE"):
+    '''get all labeled answers'''
+    with psycopg2.connect(connect_str) as conn:
+        cursor = conn.cursor()
+        sql = """SELECT DISTINCT posts.id, posts.owneruserid
+                 FROM posts
+                 INNER JOIN        
+                (SELECT DISTINCT p.postid0 AS postid
+                FROM labels
+                INNER JOIN clone_pairs_%s p
+                ON labels.postid  = p.postid0
+                AND labels.indx = p.indx0
+                AND labels.tbegin = p.tbegin0
+                AND labels.tend = p.tend0
+                WHERE %s) answers
+                ON posts.id = answers.postid
+                ;
+                """ % (tb_name_suffix, filter_str)
+        cursor.execute(sql)        
+        rows = cursor.fetchall()
+        replaced_rows = []
+        n_none_uid = 0
+        for aid, uid in rows:
+            if uid is None:
+                n_none_uid += 1
+            else:
+                replaced_rows.append((aid, uid))
+        print("there are", n_none_uid, "answers that don't have owner")
+        return replaced_rows
+
+
+def get_unique_qids(tb_name_suffix, connect_str, filter_str="TRUE"):
+    '''get all labeled answers'''
+    with psycopg2.connect(connect_str) as conn:
+        cursor = conn.cursor()
+        sql = """SELECT DISTINCT posts.parentid
+                 FROM posts
+                 INNER JOIN        
+                (SELECT DISTINCT p.postid0 AS postid
+                FROM labels
+                INNER JOIN clone_pairs_%s p
+                ON labels.postid  = p.postid0
+                AND labels.indx = p.indx0
+                AND labels.tbegin = p.tbegin0
+                AND labels.tend = p.tend0
+                WHERE %s) answers
+                ON posts.id = answers.postid
                 ;
                 """ % (tb_name_suffix, filter_str)
         cursor.execute(sql)        
@@ -118,3 +252,57 @@ def get_unique_clone_instances(tb_name_suffix, connect_str, filter_str="TRUE"):
         cursor.execute(sql)
         rows = cursor.fetchall()
         return rows
+
+
+def n_cln_clsss(filter_str):
+    with dbinfo.driver.session() as session:
+        tx = session.begin_transaction()
+        n = tx.run("MATCH (:ClnClss) return count(*)").single().value()
+        tx.commit()
+    return n
+
+def n_cln_insts(filter_str):
+    with dbinfo.driver.session() as session:
+        tx = session.begin_transaction()
+        n = tx.run("MATCH (:ClnInst) return count(*)").single().value()
+        tx.commit()
+    return n
+
+def n_question_posts(filter_str):
+    with dbinfo.driver.session() as session:
+        tx = session.begin_transaction()
+        n = tx.run("MATCH (:Question) return count(*)").single().value()
+        tx.commit()
+    return n
+
+def n_answer_posts(filter_str):
+    with dbinfo.driver.session() as session:
+        tx = session.begin_transaction()
+        n = tx.run("MATCH (:Answer) return count(*)").single().value()
+        tx.commit()
+    return n
+
+def n_answerers(filter_str):
+    with dbinfo.driver.session() as session:
+        tx = session.begin_transaction()
+        n = tx.run("MATCH (usr:User)-[:Offer]->(:Answer) return COUNT(DISTINCT usr)").single().value()
+        tx.commit()
+    return n
+
+def n_askers(filter_str):
+    with dbinfo.driver.session() as session:
+        tx = session.begin_transaction()
+        n = tx.run("MATCH (usr:User)-[:Ask]->(:Question) return COUNT(DISTINCT usr)").single().value()
+        tx.commit()
+    return n
+
+
+def test_n_summary():
+    filter_str = "TRUE"
+    assert(n_cln_clsss(filter_str) == labeled.n_cln_clsss(filter_str))
+    assert(n_cln_insts(filter_str) == labeled.n_cln_insts(filter_str))
+    assert(n_question_posts(filter_str) == labeled.n_question_posts(filter_str))
+    assert(n_answer_posts(filter_str) == labeled.n_answer_posts(filter_str))
+    assert(n_answerers(filter_str) == labeled.n_answerers(filter_str))
+    assert(n_askers(filter_str) == labeled.n_askers(filter_str))
+    labeled.print_summary()
