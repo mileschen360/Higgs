5188dd002f5684997fa8bb9d7d66a2d51abf56b5
Mengsu Chen <MengsuChen@gmail.com>
2018-09-06 22:42:27 -0400

    add statstest formatter

--- py/statstest.py
+++ py/statstest.py
@@ -3,7 +3,7 @@ import pandas as pd
 from IPython.display import display, Markdown
 
 import statistics
-
+from collections import OrderedDict
 
 from . import labeled
 
@@ -17,8 +17,8 @@ def mw(a, b, alternative, **kwds):
     m = len(a)
     n = len(b)
     if min(m,n) < 20:
-        pass
-        #print("WARNING: scipy Mann-Whitney U test work only for sample size > 20")
+        #pass
+        print("%% WARNING: scipy Mann-Whitney U test work only for sample size > 20")
     U_b, p = stats.mannwhitneyu(a, b, alternative=alternative, **kwds)
     U_a = m*n - U_b
     a_lt_b =  U_a > U_b
@@ -26,15 +26,34 @@ def mw(a, b, alternative, **kwds):
     d = 1 - 2.0*U/(m*n)
     return [p, d, a_lt_b]
 
+def group_size_mw(**kwds):
+    filter_str_secure = labeled.is_secure_clnclss()
+    filter_str_insecure = labeled.is_insecure_clnclss()
+    secure = labeled.cln_clss_sizes(filter_str_secure)
+    insecure = labeled.cln_clss_sizes(filter_str_insecure)
+    secure_mean = statistics.mean(secure)
+    insecure_mean = statistics.mean(insecure)
+    df = pd.DataFrame(index=['secure mean', 'insecure mean', 'p-value', "Cliff's d", 'result', ])
+    pds = mw(secure, insecure, **kwds)
+    if pds[-1]:
+        pds[-1] = 'secure < insecure'
+    df['group size'] = [secure_mean, insecure_mean] + pds
+    res = df.transpose()
+    return res    
 
 def mw_table(filter_str='TRUE', **kwds):
     ''''''
-    attr_name = {'users.reputation': 'reputation',
-                 'users.views':'profileviews',
-                 'answers.score':'score',
-                 'answers.commentcount':'commentcount',
-                 'questions.favoritecount':'favoritecount',
-                 'questions.viewcount':'viewcount'}
+    attr_name = OrderedDict([('answers.score', 'score'),
+                 ('answers.commentcount', 'commentcount'),
+                 ('users.reputation', 'reputation'),
+                 ('questions.favoritecount', 'favoritecount'),                 
+                 ('questions.viewcount', 'viewcount'),
+                 #('users.views', 'profileviews'),
+                 #('questions.answercount', 'answercount'),
+                 #('users.age', 'age'),
+                  ('answers.id', 'aid'),
+                  #('answers.creationdate', 'time')
+                 ])
     secure = "(%s) AND (%s) " % (filter_str, labeled.is_secure_answer())
     insecure = "(%s) AND (%s) " % (filter_str, labeled.is_insecure_answer())
 
@@ -50,6 +69,9 @@ def mw_table(filter_str='TRUE', **kwds):
         insecure_mean = statistics.mean(insecure_data)
         df[attr_name[attr]] = pds + [secure_mean, insecure_mean]
     return df.transpose()
+
+
+
 
 def interp_d(d):
     if abs(d) < 0.147:
@@ -67,7 +89,10 @@ def pretty_d(d):
     return "%.2f (%s)" % (d, effect)
 
 def pretty_p(p):
-    return round(p,2)
+    if p < 0.01:
+        return p
+    else:
+        return round(p,2)
 
 def pretty_mean(m):
     if m > 1000:
@@ -93,10 +118,10 @@ def friendly_mw_table(mw_tbl):
             sgnfc_larger.append('secure')
             
     df["larger (alpha=0.05)"] = sgnfc_larger
-    return df
+    return df.transpose()
     
 
-def mwutest(alternative):
+def mwutest(alternative, latex=False):
     cat_name = [('TRUE', 'All categories'),
     ('labels.scategory=1', 'category SSL/TLS'),
     ('labels.scategory=2', 'category Symmetric Cryptography'),
@@ -105,7 +130,32 @@ def mwutest(alternative):
     ('labels.scategory=5', 'category Random Number Generation')
     ]
 
+    latex_tb_head_0 = r"""
+%Table
+\begin{table*}[h]%%%%
+\caption{"""
+    latex_tb_head_1 =r"""}\label{tab:one}
+\begin{minipage}{\columnwidth}
+\begin{center}
+"""
+    latex_tb_tail = r"""
+\end{center}
+\bigskip\centering
+\footnotesize\emph{Source:} This is a table
+ sourcenote. This is a table sourcenote. This is a table
+ sourcenote.
+ \emph{Note:} This is a table footnote.
+\end{minipage}
+\end{table*}%%%%
+"""
+
     for cat, name in cat_name:
-        display(Markdown("## %s:" % name))
-        display(friendly_mw_table(mw_table(cat, alternative=alternative)))
+        if latex:
+            latex_tb_head = latex_tb_head_0+name+latex_tb_head_1
+            print(latex_tb_head+
+                 friendly_mw_table(mw_table(cat, alternative=alternative)).to_latex() +
+                 latex_tb_tail)
+        else:
+            display(Markdown("## %s:" % name))
+            display(friendly_mw_table(mw_table(cat, alternative=alternative)))
         print("")    
